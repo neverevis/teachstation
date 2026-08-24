2a05c1cb3fddd84fe98527d8c91742f0922b90cf
Maria Eduarda Zanetti <mariaeduardazanettimoro@gmail.com>
2026-08-24 13:18:48 -0300

    Implementação Inicial do Front-end na Página de Login e Cadastro

---
+++ src/main/java/edu/ifsp/teachstation/controller/SignUpController.java
@@ -0,0 +1,12 @@
+package edu.ifsp.teachstation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SignUpController {
+	@GetMapping("/signup")
+	public String signup() {
+		return "signup";
+	}
+}
---
+++ src/test/java/edu/ifsp/teachstation/TeachstationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifsp.teachstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachstationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
