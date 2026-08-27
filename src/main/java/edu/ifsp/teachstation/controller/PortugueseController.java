6a6d0fbe1d182917b5f60c43f82291f11df85cf9
Chris <christianamancio2011@hotmail.com>
2026-08-27 18:22:33 -0300

    feat: Implementação de portuguese e level

--- src/main/java/edu/ifsp/teachstation/controller/PortugueseController.java
+++ src/main/java/edu/ifsp/teachstation/controller/PortugueseController.java
@@ -2,10 +2,24 @@ package edu.ifsp.teachstation.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/studyarea/portuguese")
 public class PortugueseController {
-	@GetMapping("/studyarea/portuguese")
+	
+	@GetMapping("/level1")
+	public String level1() {
+		return "level1";
+	}
+	
+	@GetMapping("/level2")
+	public String level2() {
+		return "level2";
+	}
+	
+	
+	@GetMapping("")
 	public String portuguese() {
 		return "portuguese";
 	}
