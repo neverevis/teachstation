64d35f38bd066af8b3b8f4021289841bcaa868fa
gisotra <gisotra.henrique@gmail.com>
2026-08-29 22:50:11 -0300

    feat: implementação de nova classe GameObject, representando elementos visuais do mapa e classe Vector2 não finalizada, responsável pelo posicionamento individual de cada botão de navegação

---
+++ src/main/resources/static/js/map.js
@@ -0,0 +1,66 @@
+// representar pontos no espaço 
+// conceito importante do CSS -> Ele faz uso de um plano cartesiano cujo ponto (0,0) é no canto superior esquerdo da tela, então 
+// provavelmente só faremos uso de valores negativos de Y nos botões e positivos de X  
+
+let windowWidth = screen.width;
+let windowHeight = screen.height;
+
+class GameObject{
+	
+	constructor(element, position){
+		this.element = element;
+		this.position = position;
+		this.element.style.position = "absolute";
+		this.setPosition(position);
+			
+	}
+	
+	setPosition(vector2){
+		this.element.style.transform = `translate(${vector2.x}px, ${vector2.y}px)`;
+	}
+}
+
+class Vector2{
+	constructor(x, y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	getX(){
+		return this.x;
+	}
+	
+	setX(x){
+		if (Number.isFinite(x))	{
+			this.x = x;
+		} else {
+			return;
+		}
+	}
+	
+	getY(){
+			return this.y;
+		}
+		
+	setY(y){
+		if (Number.isFinite(y)){
+			this.y = y;	
+		} else {
+			return;
+		}
+	}
+		 
+	getSpacePosition(){
+		return new Vector2(this.x, this.y);
+	}
+	
+	getDistance(p1, p2){
+		//d = raíz de (x2² - x1²) + (y2² - y2²)
+		return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+	}
+	
+	
+	// o que eu vou precisar -> método de settar posição e método de pegar distância 
+	
+	
+}
